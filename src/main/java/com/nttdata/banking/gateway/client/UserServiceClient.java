8ac50857599240d1c0593f862a1611361e04c01d
mqcdev <michael.chavezjq@gmail.com>
2025-06-22 17:03:25 -0500

    feat(client): add configurations for web client

---
+++ src/main/java/com/nttdata/banking/gateway/client/UserServiceClient.java
@@ -0,0 +1,12 @@
+package com.nttdata.banking.gateway.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "userservice",  url = "http://localhost:8080", path = "/api/auth")
+public interface UserServiceClient {
+
+    @PostMapping("/validate-token")
+    void validateToken(@RequestParam String token);
+}
